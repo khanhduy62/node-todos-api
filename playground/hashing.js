03737cdd6941ddd44f22723bd9d6ea3c82d2b983
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2018-06-17 19:50:30 +0700

    hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,33 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-console.log("hash: ", hash);
+const bcrypt = require('bcryptjs');
+var password = '123abc!';
 
-var data = {
-  id: 4
-}
+bcrypt.genSalt(10, function(err, salt) {
+  bcrypt.hash(password, salt, function(err, hash) {
+      // Store hash in your password DB.
+      console.log("hash ", hash)
+  });
+});
 
-var token = jwt.sign(data, '123abc');
-console.log("token ", token)
+var hashedPassword = '$2a$10$ZnNwrrjMpHluxWI9HHYy/OoVpSi8u.eIZvBIfd2ZZLx.wBAlW/aHC';
+bcrypt.compare(password, hashedPassword, function(err, res) {
+  // res === true
+  console.log("compared ", res)
+});
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+// console.log("hash: ", hash);
 
-var decode = jwt.verify(token, '123abc')
-console.log("decode: ", decode)
+// var data = {
+//   id: 4
+// }
+
+// var token = jwt.sign(data, '123abc');
+// console.log("token ", token)
+
+// var decode = jwt.verify(token, '123abc')
+// console.log("decode: ", decode)
 // var token = {
 //   data,
 //   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const validator = require('validator');
 var mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -75,6 +76,26 @@ UserSchema.statics.findByToken = function (token) {
   })
 }
 
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+})
 var User = mongoose.model('User', UserSchema)
 
 module.exports = {User};
